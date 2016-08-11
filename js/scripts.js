532ad1c61d0b0ae81b93a9061252ab9f6c22cf6e
Kyle Lange and Marta Sánchez <marta@>
2016-08-11 13:12:32 -0700

    maybe this replaced the other thing
    second comment to test this rebase stuff
    
    testing new commit message

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,29 @@
 
 
 // User Interface Logic
+$(document).ready(function () {
+  $("form").submit(function(event) {
+    var tax = $("#tax").val();
+    var model = $("#model").val();
+    var gold = $("#gold").val();
+
+    if (tax === "tax-yes" && model === "model-no" && gold === "gold-no") {
+      $("#ronaldo").hide();
+      $("#messi").show();
+
+    }
+      else if (tax === "tax-no" && model === "model-yes" && gold === "gold-yes") {
+      $("#messi").hide();
+      $("#ronaldo").show();
+      }
+      else {
+        $("#messi").hide();
+        $("#ronaldo").show();
+      }
+
+    event.preventDefault();
+  });
+
+});
+
+console.log("program complete");
